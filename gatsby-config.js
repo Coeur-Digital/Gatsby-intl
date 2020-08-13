6244b78a0d3c6c3224a0b325dcb54b7e6d09fabf
Bastien <bastien@coeur-digital.com>
2020-08-13 13:32:23 +0700

    Styled + Navigation

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,19 +2,75 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `@gatsbyjs`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        displayName: true,
+        pure: true
+      }
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
+        path: `${__dirname}/src/images`
+      }
+    },{
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Oswald`,
+            subsets: [`latin`],
+          },
+          {
+            family: `Open Sans`,
+            variants: [`400`, `700`]
+          },
+        ],
       },
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    {
+      resolve: "gatsby-plugin-prettier-eslint",
+      // this is the default configuration, override only what you need
+      options: {
+        cwd: process.cwd(), // path to a directory that should be considered as the current working directory
+        watch: true, // format/lint on save
+        initialScan: true, // if true, will format/lint the whole project on Gatsby startup
+        onChangeFullScanLint: false, // if true, on file save always perform full scan lint
+        onChangeFullScanFormat: false, // if true, on file save always perform full scan format
+        prettierLast: false, // if true, will run Prettier after ESLint
+        ignorePatterns: [
+          "**/node_modules/**/*",
+          "**/.git/**/*",
+          "**/dist/**/*",
+          ".cache/**/*",
+          "public/**/*",
+        ], // string or array of paths/files/globs to ignore
+        prettier: {
+          patterns: [], // string or array of paths/files/globs to include related only to Prettier
+          ignorePatterns: [], // string or array of paths/files/globs to exclude related only to Prettier
+          customOptions: {}, // see: https://prettier.io/docs/en/options.html
+        },
+        eslint: {
+          patterns: [], // string or array of paths/files/globs to include related only to ESLint
+          ignorePatterns: [], // string or array of paths/files/globs to exclude related only to ESLint
+          formatter: "stylish", // set custom or third party formatter
+          maxWarnings: undefined, // number of max warnings allowed, when exceed it will fail Gatsby build
+          emitWarning: true, // if true, will emit lint warnings
+          failOnError: false, // if true, any lint error will fail the build, you may set true only in your prod config
+          failOnWarning: false, // same as failOnError but for warnings
+          plugins: [], // an array of plugins to load for ESLint
+          customOptions: {}, // see: https://eslint.org/docs/developer-guide/nodejs-api#cliengine
+        },
+      },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -24,8 +80,8 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
+        icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
+      }
     },
     {
       resolve: `gatsby-plugin-intl`,
@@ -34,11 +90,11 @@ module.exports = {
         languages: [`en`, `ko`, `de`],
         defaultLanguage: `en`,
         redirect: true,
-        redirectComponent: require.resolve(`./src/components/redirect.js`),
-      },
-    },
+        redirectComponent: require.resolve(`./src/components/redirect.js`)
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-  ],
-}
+  ]
+};
