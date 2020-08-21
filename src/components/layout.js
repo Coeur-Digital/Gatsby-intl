bdf6175d9b4940811e52b8fd773352222fd43f16
Bastien <bastien@coeur-digital.com>
2020-08-21 13:22:50 +0700

    Reset

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,13 +5,13 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
-import { injectIntl } from "gatsby-plugin-intl"
+import React from 'react'
+import PropTypes from 'prop-types'
+import { useStaticQuery, graphql } from 'gatsby'
+import { injectIntl } from 'gatsby-plugin-intl'
 
-import Header from "./header"
-import "./layout.css"
+import Header from './header'
+import './layout.css'
 
 const Layout = ({ children, intl }) => {
   const data = useStaticQuery(graphql`
@@ -26,22 +26,13 @@ const Layout = ({ children, intl }) => {
 
   return (
     <>
-      <Header siteTitle={intl.formatMessage({ id: "title" })} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
+      <Header siteTitle={intl.formatMessage({ id: 'title' })} />
+
+      <main>{children}</main>
+      <footer>
+        © {new Date().getFullYear()}, Built with
+        <a href="https://www.gatsbyjs.org">Gatsby</a>
+      </footer>
     </>
   )
 }
